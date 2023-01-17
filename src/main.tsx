2f44adb543ff7b7a1bb95d0ee6c65e9f0726d3d4
jjribeiro1 <jvsr.99@hotmail.com>
2023-01-17 20:48:07 -0300

    feat: add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import { GlobalStyle } from './styles/global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 );

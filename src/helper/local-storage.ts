df85edfcb20d75da2dfa11d0e9544a097071f4e4
jjribeiro1 <jvsr.99@hotmail.com>
2023-01-22 23:05:36 -0300

    feat: add LocalStorageHelper

---
+++ src/helper/local-storage.ts
@@ -0,0 +1,20 @@
+const createLocalStorageHelper = () => ({
+  getItem(key: string) {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
+  },
+
+  setItem(key: string, value: any) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+
+  removeItem(key: string) {
+    localStorage.removeItem(key);
+  },
+
+  clear() {
+    localStorage.clear();
+  },
+});
+
+export const LocalStorageHelper = createLocalStorageHelper();

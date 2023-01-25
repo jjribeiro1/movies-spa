a301ac504c3c501fb967f669090e0364a9a360c9
jjribeiro1 <jvsr.99@hotmail.com>
2023-01-25 02:06:31 -0300

    feat: add request interceptor

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,20 @@
 import axios from 'axios';
+import { LocalStorageHelper } from '../helper/local-storage';
+const bearerToken = `Bearer ${LocalStorageHelper.getItem('access_token')}`;
 
 export const api = axios.create({
   baseURL: 'http://localhost:3000',
 });
+
+api.interceptors.request.use(
+  function (config) {
+    config.headers = {
+      Authorization: bearerToken,
+    };
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);

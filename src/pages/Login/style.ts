a248f8cad99e73a79490b55680edd66bbda162d9
jjribeiro1 <jvsr.99@hotmail.com>
2023-02-03 19:18:56 -0300

    refactor: responsive login

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -2,53 +2,73 @@ import styled, { css } from 'styled-components';
 import * as Dialog from '@radix-ui/react-dialog';
 
 export const LoginSection = styled.section`
-  ${({ theme }) => css`
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    background-color: ${theme.colors.pageBg};
-  `}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  height: 100%;
+  max-width: 1440px;
 `;
 
 export const LoginForm = styled.form`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
     padding: 1.5rem;
-    width: 360px;
-    height: 380px;
+    width: 380px;
+    height: 400px;
     border-radius: 10px;
-    background-color: ${theme.colors.lightBg};
+    background-color: ${theme.colors.lightBg3};
   `}
+
+  @media screen and (width < 768px) {
+    width: 340px;
+    height: 360px;
+    padding: 1.2rem;
+  }
+
+  @media screen and (width < 425px) {
+    width: 300px;
+    height: 360px;
+    padding: 1.2rem;
+  }
 `;
 
 export const FormTitle = styled.h2`
   ${({ theme }) => css`
-    text-align: center;
     font-weight: 600;
     font-size: 2rem;
     color: ${theme.colors.c1};
   `}
+
+  @media screen and (width < 768px) {
+    font-size: 1.5rem;
+  }
 `;
 
 export const FormControls = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
+    width: 80%;
     gap: 0.3rem;
-    margin: 1.2rem 1.2rem;
+    margin: 1.2rem 0;
 
     label {
       font-size: 1.3rem;
       font-weight: 600;
       color: ${theme.colors.c4};
+
+      @media screen and (width < 768px) {
+        font-size: 1rem;
+      }
     }
 
     input {
       padding: 0.6rem;
+      width: 100%;
       height: 1.8rem;
       font-size: 1.2rem;
       border-radius: 5px;
@@ -59,15 +79,10 @@ export const FormControls = styled.div`
 
 export const SubmitButton = styled.button`
   ${({ theme }) => css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-self: center;
-    padding: 0.3rem;
-    margin-top: 1.5rem;
-    width: 5.5rem;
-    height: 2.5rem;
-    border-radius: 6px;
+    max-width: fit-content;
+    padding: 0.2rem 2rem;
+    margin: 1rem 0;
+    border-radius: 5px;
     font-size: 1rem;
     font-weight: 400;
     color: ${theme.colors.c3};
@@ -81,16 +96,19 @@ export const SubmitButton = styled.button`
 
 export const Register = styled.div`
   ${({ theme }) => css`
+    font-size: 1rem;
     display: flex;
     gap: 1rem;
-    margin: 1.4rem auto;
     color: ${theme.colors.c2};
   `}
+  @media screen and (width < 425px) {
+    font-size: 0.8rem;
+  }
 `;
 
 export const DialogTrigger = styled(Dialog.Trigger)`
   ${({ theme }) => css`
-    background-color: ${theme.colors.lightBg};
+    background-color: inherit;
     color: ${theme.colors.c5};
     border-style: none;
     :hover {

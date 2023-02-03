ff345e50702e14a39e64e8456cc321097355f153
jjribeiro1 <jvsr.99@hotmail.com>
2023-02-02 22:44:14 -0300

    refactor: css reset

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,15 +1,30 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-* {
+*, *::before, *::after {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
 html, body, #root {
-  width: 100%;
-  height: 100vh;
-  background-color: #050c17;
+  ${({ theme }) => css`
+    background-color: ${theme.colors.pageBg};
+  `}
+  height: 100%;
+}
+body {
+  line-height: 1.5;
+}
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+input, button, textarea, select {
+  font: inherit;
+}
+
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
 }
 `;

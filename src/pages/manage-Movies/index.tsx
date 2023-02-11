0f49af765f5190feccc4adaf3fe6a2f99d2e32a9
jjribeiro1 <jvsr.99@hotmail.com>
2023-02-11 20:53:41 -0300

    feat: add validations to manage movies page

--- src/pages/manage-Movies/index.tsx
+++ src/pages/manage-Movies/index.tsx
@@ -4,7 +4,7 @@ import { RadixDialog } from '../../components/radix-dialog';
 import { ToastMessage } from '../../components/radix-toast';
 import { MovieForm } from '../../components/movie-form';
 import { MovieService } from '../../services/movies-service';
-import { CreateMovieInput, Movie } from '../../types/movie-service-types';
+import { CreateMovieInput, Movie, UpdateMovieInput } from '../../types/movie-service-types';
 import {
   ManageSection,
   MovieListTitle,
@@ -35,17 +35,32 @@ export function ManageMovies() {
     }
   }
 
-  async function handleCreateMovieSubmit(e: FormEvent<HTMLFormElement>) {
+  function handleCreateMovieSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    const error: string[] = [];
+    const data: CreateMovieInput = {
+      name: e.currentTarget.Name.value,
+      imageUrl: e.currentTarget.imageUrl.value,
+      ageRating: parseInt(e.currentTarget.ageRating.value),
+      releaseYear: parseInt(e.currentTarget.releaseYear.value),
+      genreIds: [e.currentTarget.genreIds.value],
+      streamingIds: [e.currentTarget.streamingIds.value],
+    };
+
+    data.name.length >= 3 ? null : error.push('Nome precisa ter pelo menos 3 caracteres');
+    data.ageRating >= 0 ? null : error.push('Classificação precisa ser maior que 0');
+    data.releaseYear >= 0 ? null : error.push('Ano de lançamento precisa ser maior que 0');
+
+    if (error.length > 0) {
+      setToastMessage(error);
+      setOpenToast(true);
+      return;
+    }
+    createMovie(data);
+  }
+
+  async function createMovie(data: CreateMovieInput) {
     try {
-      const data: CreateMovieInput = {
-        name: e.currentTarget.Name.value,
-        imageUrl: e.currentTarget.imageUrl.value,
-        ageRating: parseInt(e.currentTarget.ageRating.value),
-        releaseYear: parseInt(e.currentTarget.releaseYear.value),
-        genreIds: [e.currentTarget.genreIds.value],
-        streamingIds: [e.currentTarget.streamingIds.value],
-      };
       await MovieService().create(data);
       setControl(!control);
       setOpenCreateMovieModal(false);
@@ -55,25 +70,40 @@ export function ManageMovies() {
     }
   }
 
-  async function handleUpdateMovieSubmit(e: FormEvent<HTMLFormElement>) {
+  function handleUpdateMovieSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    try {
-      const data = {
-        name:
-          e.currentTarget.Name.value === movieToUpdate.name
-            ? undefined
-            : e.currentTarget.Name.value,
+    const error: string[] = [];
+    const data: CreateMovieInput = {
+      name:
+        e.currentTarget.Name.value === movieToUpdate.name ? undefined : e.currentTarget.Name.value,
 
-        imageUrl:
-          e.currentTarget.imageUrl.value === movieToUpdate.imageUrl
-            ? undefined
-            : e.currentTarget.imageUrl.value,
-        ageRating: parseInt(e.currentTarget.ageRating.value),
-        releaseYear: parseInt(e.currentTarget.releaseYear.value),
-        genreIds: [e.currentTarget.genreIds.value],
-        streamingIds: [e.currentTarget.streamingIds.value],
-      };
-      await MovieService().update({ id: movieToUpdate.id, ...data });
+      imageUrl:
+        e.currentTarget.imageUrl.value === movieToUpdate.imageUrl
+          ? undefined
+          : e.currentTarget.imageUrl.value,
+      ageRating: parseInt(e.currentTarget.ageRating.value),
+      releaseYear: parseInt(e.currentTarget.releaseYear.value),
+      genreIds: [e.currentTarget.genreIds.value],
+      streamingIds: [e.currentTarget.streamingIds.value],
+    };
+
+    if (data.name) {
+      data.name.length >= 3 ? null : error.push('Nome precisa ter pelo menos 3 caracteres');
+    }
+    data.ageRating > 0 ? null : error.push('Classificação precisa ser maior que 0');
+    data.releaseYear > 0 ? null : error.push('Ano de lançamento precisa ser maior que 0');
+
+    if (error.length > 0) {
+      setToastMessage(error);
+      setOpenToast(true);
+      return;
+    }
+    updateMovie({ id: movieToUpdate.id, ...data });
+  }
+
+  async function updateMovie(data: UpdateMovieInput) {
+    try {
+      await MovieService().update(data);
       setControl(!control);
       setOpenEditMovieModal(false);
     } catch (error: any) {

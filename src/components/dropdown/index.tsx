fd18d3576cda3d79517e45e1e0938e2f78d83e19
jjribeiro1 <jvsr.99@hotmail.com>
2023-02-14 20:54:20 -0300

    feat: only admin can manage page

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { LocalStorageHelper } from '../../helper/local-storage';
 import { AuthService } from '../../services/auth-service';
 import { DropdownGroup, DropdownContent, DropdownMenuItem } from './style';
 
@@ -8,19 +10,31 @@ export function Dropdown() {
     AuthService().logOut();
     navigate('/');
   }
+  const [isAdmin, setIsAdmin] = useState(false);
+  useEffect(() => {
+    const userRole = LocalStorageHelper.getItem('logged_user').role;
+    userRole === 'ADMIN' ? setIsAdmin(true) : null;
+  }, []);
+
   return (
     <>
-      <DropdownContent side='bottom' sideOffset={10} align='end' alignOffset={20}>
+      <DropdownContent side="bottom" sideOffset={10} align="end" alignOffset={20}>
         <DropdownGroup>
-          <DropdownMenuItem onClick={() => navigate('/manage-genres')}>
-            Gerenciar gêneros
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => navigate('/manage-streamings')}>
-            Gerenciar streamings
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => navigate('/manage-movies')}>
-            Gerenciar filmes
-          </DropdownMenuItem>
+          <></>
+          {isAdmin && (
+            <>
+              <DropdownMenuItem onClick={() => navigate('/manage-genres')}>
+                Gerenciar gêneros
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => navigate('/manage-streamings')}>
+                Gerenciar streamings
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => navigate('/manage-movies')}>
+                Gerenciar filmes
+              </DropdownMenuItem>
+            </>
+          )}
+
           <DropdownMenuItem onClick={() => navigate('/profile')}>Perfis</DropdownMenuItem>
           <DropdownMenuItem onClick={logOut}>Sair</DropdownMenuItem>
         </DropdownGroup>
